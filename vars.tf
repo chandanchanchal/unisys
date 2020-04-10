86c84806986e5d3e96da90ba38041e9256964f6d
chandanchanchal <34713852+chandanchanchal@users.noreply.github.com>
2020-04-10 14:44:09 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,16 +1,14 @@
-variable "AWS_ACCESS_KEY" {default = "AKIAJG4O"}
-variable "AWS_SECRET_KEY" {default = "rYN3mDHJbAdOFe8R2B9XEa6rl7LHNp"}
+variable "AWS_ACCESS_KEY" {default = "AKIA3ILMKNKHHLAGVMVG"}
+variable "AWS_SECRET_KEY" {default = "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"}
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "ap-south-1"
 }
 variable "AMIS" {
   type = "map"
   default = {
     us-east-1 = "ami-13be557e"
     us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    ap-south-1 = "ami-04b2519c83e2a7ea5"
   }
 }
 
-
-#
